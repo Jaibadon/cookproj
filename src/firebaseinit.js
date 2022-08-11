1bae2b1cc9cb972668f7fa48151bfd173e61b98a
Jai Nielsen <jai@nielsen.org.nz>
2022-08-12 10:39:08 +1200

    More database functionality

--- src/firebaseinit.js
+++ src/firebaseinit.js
@@ -4,7 +4,7 @@ import { getDatabase, ref, update, runTransaction, onValue, query, orderByKey, e
 import { getStorage, uploadBytes, getBytes, ref as sRef } from "firebase/storage";
 import 'bootstrap';
 
-
+var completedimageload = false;
 
 function sLargeRc(shown){
   if(shown.match("enrec") != null){
@@ -20,9 +20,11 @@ function sLargeRc(shown){
     document.getElementById('enlargedrecipe').style.display='block';
   
   
-    let storRef = sRef(storage, 'recipedata/' + shown.split(/enrec/)[1]);
+    let storRef = sRef(storage, 'recipedata/' + shown.split(/enrec/)[1] + "/save");
+
+    let docengref = document.getElementById("enlargedrecipe");
   
-    getBytes(storRef).then((arraybuf) => rWrec(arraybuf));
+    getBytes(storRef).then((arraybuf) => rWrec(arraybuf, true, docengref));
   
   
     return false;
@@ -32,35 +34,30 @@ function sLargeRc(shown){
 
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  var ahash = window.location.hash.slice(1);
-
-    if(window.location.hash == ""){
-        show('home');
-    }
-    else{
-            if(ahash == 'game'){
-               show('game');
-            }
-            else if(ahash == 'home'){
-                show('home');
-            }
-            else if(ahash == 'about'){
-                show('about');
-            }
-            else if(ahash == 'recipes'){
-                show('recipes');
-            }
-            else if(ahash.match("enrec") != null){
-              sLargeRc(ahash)
-            }
-            else {
-              show('home')
-            }
-    }
+  hasherFunc();
 });
 
 
+document.getElementById("uploadButton").onclick = () => {
+  let fileElement = document.getElementById('fileInput')
+
+  if (fileElement.files.length === 0) {
+    alert('Please choose a file')
+    return
+  }
+
+  let file = fileElement.files[0];
+
+  writeRecipe("lasagne", "noice lasagne", "15mins", "AHSHSHSHS \n eat the beans", "beans and rice", file);
+
+}
+
+
 window.addEventListener('hashchange', function() {
+  hasherFunc();
+});
+
+function hasherFunc(){
   var ahash = window.location.hash.slice(1);
 
   if(window.location.hash == ""){
@@ -79,16 +76,16 @@ window.addEventListener('hashchange', function() {
           else if(ahash == 'recipes'){
               show('recipes');
           }
+          else if(ahash == 'editRecipe'){
+            show('editRecipe');
+          }
           else if(ahash.match("enrec") != null){
             sLargeRc(ahash)
           }
           else {
             show('home')
           }
-  }
-});
-
-
+  }}
 
 
 const firebaseConfig = {
@@ -112,6 +109,8 @@ var signedin = false;
 
 var userid = null;
 
+var isadmin = false;
+
 const auth = getAuth();
 
 const whenSignedIn = $('[id=whenSignedIn]');
@@ -133,13 +132,19 @@ onAuthStateChanged(auth, (user) => {
     if (user) {
         signedin = true;
         userid = user.uid;
+        if(userid == "H0G3Y9tBJxTlrePksO8iCh9CaSH3"){
+          isadmin = true;
+        }
         whenSignedIn.show();
         whenSignedOut.hide();
     } else {
+        isadmin = false;
         whenSignedIn.hide();
         whenSignedOut.show(); 
     }
 });
+
+
 
 onValue(ref(db, 'recipes'), (snapshot) => {
   createRec(snapshot.size, snapshot.ref);
@@ -150,9 +155,23 @@ onValue(ref(db, 'recipes'), (snapshot) => {
 
       var viewbutton = document.getElementById("view_recipe" + n);
       var upvotebutton = document.getElementById("upvote" + n);
+      if(isadmin == true){
+        var editbutton = document.getElementById("edit_recipe" + n);
+        editbutton.onclick = function(){show('editRecipe')};
+      }
 
       viewbutton.onclick = function(){sLargeRc('enrec' + childSnapshot.key)};
+      
       upvotebutton.onclick = function(){upvoteRecipe(childSnapshot.key, userid)};
+
+      if(completedimageload == false){
+        let storRef = sRef(storage, 'recipedata/' + childSnapshot.key + "/image");
+
+        var imageelement = document.getElementById("recipe_image" + n)
+      
+        getBytes(storRef).then((arraybuf) => rWrec(arraybuf, false, imageelement));  
+        completedimageload = true;
+      }
 
 
       childSnapshot.forEach((ccduSnapshot) =>{
@@ -189,28 +208,34 @@ for(var i = 0; i < recinum; i++){
   colDiv.appendChild(cardDiv);
 
   document.getElementById("recipescontainer").append(colDiv);
-
-  var imagerecipe = document.createElement("img");
-
-  imagerecipe.id = ("recipe_image" + i);
-
-  cardDiv.appendChild(imagerecipe);
-
+//build up string to go in innerhtml
   cardDiv.innerHTML += `
   <div class="card-body">
       <p class="card-text" id="recipe_name` + i + `"></p>
+      <small class="text-muted" id="recipe_time` + i + `"></small>
       <div class="d-flex justify-content-between align-items-center">
         <div class="btn-group">
           <button type="button" class="btn btn-sm btn-outline-secondary" id="view_recipe` + i + `">View</button>
           <button type="button" class="btn btn-sm btn-outline-secondary" id="edit_recipe` + i + `">Edit</button>
           <button type="button" class="btn btn-sm btn-outline-secondary" id="upvote` + i + `">Upvote</button>
-          <small>Upvotes: </small><small id="upvotes` + i + `"></small>
-          <small class="text-muted" id="recipe_time` + i + `"></small>
+         <small id="upvotes` + i + `"></small>
         </div>
-        
       </div>
     </div>
   `
+  if(completedimageload == false){
+    var imagerecipe = document.createElement("img");
+
+    imagerecipe.id = ("recipe_image" + i);
+
+    cardDiv.appendChild(imagerecipe);
+  }
+
+if(isadmin != true){
+  document.getElementById('edit_recipe').remove();
+}
+
+
   
 //sort recipes into official and non-offical. The official ones you can play on the minigame.
 
@@ -220,23 +245,28 @@ for(var i = 0; i < recinum; i++){
 
 }
 }
-function writeRecipe(recipe_shorthand, recipe_name, recipe_time, recipe_ingredients, recipe_instructions){
+function writeRecipe(recipe_shorthand, recipe_name, recipe_time, recipe_ingredients, recipe_instructions, recipe_image){
     const recref = ref(db, 'recipes/' + recipe_shorthand);
-    const storRef = sRef(storage, 'recipedata/' + recipe_shorthand);
     
     update( recref, {
         recipe_name: recipe_name,
         recipe_time: recipe_time
         });
 
-      var savefile = new Blob(["<div id='erecipe_instructions'>" + recipe_instructions + "</div><div id='erecipe_ingredients'>" + recipe_ingredients + "</div>"], {type: "text/html"});
+      var savefile = new File(["<div id='erecipe_instructions'>" + recipe_instructions + "</div><div id='erecipe_ingredients'>" + recipe_ingredients + "</div>"],recipe_shorthand + "save", {type: "text/html"});
 
-      uploadBytes(storRef, savefile).then((snapshot) => {
+        
+      uploadBytes(sRef(storage, 'recipedata/' + recipe_shorthand + "/image"), recipe_image).then((snapshot) => {
+        console.log('Uploaded image');
+      });
+
+      uploadBytes(sRef(storage, 'recipedata/' + recipe_shorthand + "/save"), savefile).then((snapshot) => {
         console.log('Uploaded instructions and ingredients');
       });
+      
     }
 
-//writeRecipe("lasagne", "noice lasagne", "15mins", "cook the beans in the beans nicely \n eat the beans", "beans and rice");
+
 
 
 
@@ -285,12 +315,25 @@ function upvoteRecipe(recipe_shorthand, userid){
 
 
 
-function rWrec(recbuffer){
-    let recblob = new Blob([recbuffer], {type : "text/plain"});
+function rWrec(recbuffer, boolsav, htmname){
+    if(boolsav== true){
+      let recblob = new Blob([recbuffer], {type : "text/plain"});
+      (async () => {
+        htmname.innerHTML = await recblob.text();
+      })();
+    }else{
+      
+      (async () => {
+        let recblob = new Blob([recbuffer], {type : "img/png"});
+        var urlCreator = window.URL || window.webkitURL;
+        var imageUrl = urlCreator.createObjectURL(recblob);
+        htmname.src = imageUrl;
+      })();
 
-    (async () => {
-      document.getElementById("enlargedrecipe").innerHTML = await recblob.text();
-    })();
+    }
+    
+
+    
     
           
 }
